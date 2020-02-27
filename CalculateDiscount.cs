236857f96562487143b5fac009f0e22862837266
Nishan Perera <nishanperera@tfl.gov.uk>
2020-02-27 23:38:08 +0000

    Initial commit

---
+++ CalculateDiscount.cs
@@ -0,0 +1,21 @@
+﻿namespace FeatureToggle
+{
+    public interface ICalculateDiscount
+    {
+        double ApplyDiscount();
+    }
+
+    public class CalculateDiscount : ICalculateDiscount
+    {
+        public double Discount { get; set; }
+        public CalculateDiscount()
+        {
+            Discount = .1;
+        }
+
+        public double ApplyDiscount()
+        {
+            return .1;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace FeatureToggle.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public bool FeatureA { get; set; }
+        public bool FeatureB { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IFeatureManager _featureManager;
+        private readonly Lazy<ICalculateDiscount> _calculateDiscount;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            IFeatureManager featureManager,
+            Lazy<ICalculateDiscount> calculateDiscount)
+        {
+            _logger = logger;
+            _featureManager = featureManager;
+            _calculateDiscount = calculateDiscount;
+        }
+
+        public async void OnGet()
+        {
+            FeatureA = await _featureManager.IsEnabledAsync("FeatureA");
+            if (FeatureA)
+            {
+                _logger.LogInformation("Calling dependency.");
+                _calculateDiscount.Value.ApplyDiscount();
+            }
+            FeatureB = await _featureManager.IsEnabledAsync("FeatureB");
+        }
+    }
+}
